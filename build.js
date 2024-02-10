44348ed93423b15a1654bde4530f313327ed0391
calasanmarko <calasanmarko@hotmail.com>
2024-02-10 02:33:28 +0100

    Improvements

--- build.js
+++ build.js
@@ -1,5 +1,5 @@
 const path = require('path')
-const feather = require('./icons.json')
+const material = require('material-icons-json/dist/icons.json')
 const { pascalCase } = require('pascal-case')
 const fs = require('fs-extra')
 
@@ -8,7 +8,7 @@ const handleComponentName = name => name.replace(/\-(\d+)/, '$1').match(/^\d/) ?
 const component = (icon) =>
 `<script>
   export let size = "24";
-  export let strokeWidth = 2;
+  export let strokeWidth = 0;
   let customClass = "";
   export { customClass as class };
 
@@ -19,25 +19,21 @@ const component = (icon) =>
   }
 </script>
 
-<svg xmlns="http://www.w3.org/2000/svg" width={size} height={size} fill="none" viewBox="0 0 24 24"  stroke="currentColor" stroke-width="{strokeWidth}" stroke-linecap="round" stroke-linejoin="round" class="feather feather-${icon.name} {customClass}">${feather[icon.name]}</svg>
+<svg xmlns="http://www.w3.org/2000/svg" width={size} height={size} fill="currentColor" viewBox="0 0 24 24" stroke="currentColor" stroke-width="{strokeWidth}" class="material material-${icon.name} {customClass}">${material[icon.name]}</svg>
 `
 
-const icons = Object.keys(feather).map(name => ({
+const icons = Object.keys(material).map(name => ({
   name,
   pascalCasedComponentName: pascalCase(`${handleComponentName(name)}-icon`),
   kebabCasedComponentName: `${handleComponentName(name)}-icon`
 }))
 
 Promise.all(icons.map(icon => {
-  const filepath = `./src/icons/${icon.pascalCasedComponentName}.svelte`
-  return fs.ensureDir(path.dirname(filepath))
-    .then(() => fs.writeFileSync(filepath, component(icon), 'utf8'))
-})).then(async () => {
-  const main = icons
-    .map(icon => `export { default as ${icon.pascalCasedComponentName} } from './icons/${icon.pascalCasedComponentName}.svelte'`)
-    .join('\n\n')
-  const types = '/// <reference types="svelte" />\nimport {SvelteComponentTyped} from "svelte/internal"\n' +
-    icons.map(icon => `export class ${icon.pascalCasedComponentName} extends SvelteComponentTyped<{size?: string, strokeWidth?: number, class?: string}> {}`).join("\n")
-  await fs.outputFile("index.d.ts", types, 'utf8');
-  return await fs.outputFile('./src/index.js', main, 'utf8')
-})
+  const baseFilepath = `./dist/${icon.pascalCasedComponentName}`;
+  const svelteFilepath = `${baseFilepath}.svelte`;
+  const typeFilepath = `${baseFilepath}.d.ts`;
+
+  return fs.ensureDir(path.dirname(baseFilepath))
+    .then(() => fs.writeFileSync(svelteFilepath, component(icon), 'utf8'))
+    .then(() => fs.writeFileSync(typeFilepath, '/// <reference types="svelte" />\nimport {SvelteComponentTyped} from "svelte/internal"\nexport class ' + icon.pascalCasedComponentName + ' extends SvelteComponentTyped<{size?: string, strokeWidth?: number, class?: string}> {}', 'utf8'))
+}));
